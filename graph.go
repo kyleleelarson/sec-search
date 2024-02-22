1d681738ddbe34f795227e36a693e9fa870a4c4c
Kyle Larson <kyleleelarson@gmail.com>
2024-02-22 15:52:06 +0100

    break out search functionality

--- graph.go
+++ graph.go
@@ -1,88 +1,22 @@
 package main
 
 import (
-  "log"
   "os"
   "io"
   "fmt"
   "bytes"
   "net/http"
   "html/template"
-  "encoding/json"
-  "strings"
-  "github.com/elastic/go-elasticsearch/v8"
 	"github.com/go-echarts/go-echarts/v2/charts"
 	"github.com/go-echarts/go-echarts/v2/opts"
   chartrender "github.com/go-echarts/go-echarts/v2/render"
 )
 
-const indexName = "filings"
+var client *ElasticClient
 var templates = template.Must(template.ParseFiles("table.html"))
 var fields = [2]string {"Item1","Item1a"}
 var years = [20]string {"2004","2005","2006","2007","2008","2009","2010","2011","2012","2013",
                         "2014","2015","2016","2017","2018","2019","2020","2021","2022","2023"}
-
-var es *elasticsearch.Client
-
-var histogramQuery = `{ 
-        "size": 0,
-        "query": { 
-              "bool": { 
-                "must": [{ "match_phrase": { "%s": "%s" }}],
-                "filter": [{ "bool": { "must": [{ "term": { "StockIndex.keyword": "%s" }}]}}]}},
-        "aggs": { "year": { "date_histogram": { "field": "Filed", "calendar_interval": "1y"}}}
-    }`
-
-
-type HistogramResult struct {
-  Took float64 `json:"took"`
-  Hits struct {
-    Total struct {
-      Num int `json:"value"`
-    } `json:"total"`
-  }
-  Aggregations struct {
-    Year struct {
-      Buckets []struct {  
-        Date  string  `json:"key_as_string"`
-        Count float64 `json:"doc_count"`
-      } `json:"buckets"`
-    } `json:"year"`
-  } `json:"aggregations"`
-}
-var highlightQuery = `{ 
-              "_source": ["Ticker", "Name", "StockIndex", "Filed"],
-              "query": { 
-                    "bool": { 
-                      "must": [{ "match_phrase": { "%s": "%s" }}],
-                      "filter": [{ "bool": { "must": [{ "term": { "StockIndex.keyword": "%s" }}]}}]}},
-              "highlight": { "fragment_size": 200, "fields": { "Item1": {} } },
-              "sort": [ { "Filed": { "order": "desc", "unmapped_type": "date" } } ],
-              "size": 30
-            }`
-
-type ResultBody struct {
-  Took float64 `json:"took"`
-  Hits struct {
-    Total struct {
-      Num int `json:"value"`
-    } `json:"total"`
-    Values []struct {  
-      Id     string  `json:"_id"`
-      Score  float64 `json:"_score"`
-      Source struct {
-        Ticker     string
-        Name       string
-        StockIndex string
-        Filed      string
-      } `json:"_source"`
-      Highlights struct {
-        Item1  []template.HTML
-        Item1a []template.HTML
-      } `json:"highlight"`
-    } `json:"hits"`
-  } `json:"hits"`
-}
 
 type embedRender struct {
   c      interface{}
@@ -114,80 +48,25 @@ func (r *embedRender) Render(w io.Writer) error {
   return err
 }
 
-func client_init() {
-  var err error
-
-  cfg := elasticsearch.Config {
-    Addresses: []string {
-      "https://localhost:9200",
-    },
-    Username: "elastic",
-    Password: os.Getenv("ES_PASS"),
-    CertificateFingerprint: os.Getenv("ES_CERTFP"),
-  }
-
-  es, err = elasticsearch.NewClient(cfg)
-  if err != nil {
-    log.Fatalf("Error creating client: %s", err)
-  }
-}
-
-
-
 func httpserver(w http.ResponseWriter, r *http.Request) {
+  var buf bytes.Buffer
+  barData := make(map[string]([]opts.BarData))
+
   searchTerm := r.FormValue("searchterm")
-  barData    := make(map[string][]opts.BarData)
   stockIndex := "S&P 500"
 
+  counts, err := client.histogramSearch(searchTerm, stockIndex)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
+  }
+
   for _, field := range fields {
-    res, err := es.Search(
-      es.Search.WithIndex(indexName),
-      es.Search.WithBody(strings.NewReader(fmt.Sprintf(histogramQuery, field, searchTerm, stockIndex))),
-    )
-    if err != nil {
-      log.Fatalf("Error getting response: %s", err)
+    var barValues []opts.BarData
+    for _, year := range years {
+      count := counts[field][year] // zero if not in map
+      barValues = append(barValues, opts.BarData{Value: count})
     }
-    defer res.Body.Close()
-    if res.IsError() {
-      var e map[string]interface{}
-      if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-        log.Fatalf("Error parsing the response (with error) body: %s", err)
-      } else {
-        // Print the response status and error information.
-        log.Fatalf("res.IsError [%s] %s: %s",
-          res.Status(),
-          e["error"].(map[string]interface{})["type"],
-          e["error"].(map[string]interface{})["reason"],
-        )
-      }
-    }
-
-    if res.Status() == "200 OK" {
-      body, err := io.ReadAll(res.Body)
-      if err != nil {
-        log.Fatalf("Error reading the response body: %s", err)
-      }
-      var result HistogramResult
-      if err = json.Unmarshal(body, &result); err != nil {
-        log.Fatalf("Error parsing the response body: %s", err)
-      }
-      log.Printf("For field %s, ", field)
-      log.Printf("took: %v ms, ", result.Took)
-      log.Printf("hits: %d\n", result.Hits.Total.Num)
-
-      counts := make(map[string]int)
-      for _, b := range result.Aggregations.Year.Buckets {
-        year := b.Date[:4]
-        count := int(b.Count)
-        //log.Printf("  date: %s, count: %v\n", year, count)
-        counts[year] = count
-      }
-
-      for _, year := range years {
-        count := counts[year] // zero if not in map
-        barData[field] = append(barData[field], opts.BarData{Value: count})
-      }
-    }
+      barData[field] = barValues
   }
 
 	// create a new bar instance
@@ -207,68 +86,26 @@ func httpserver(w http.ResponseWriter, r *http.Request) {
 		  Stack: "stackA",
 		}))
 
-  var buf bytes.Buffer
-
-  err := bar.Render(&buf)
+  err = bar.Render(&buf)
   if err != nil {
-    log.Fatalf("Error rendering into buffer: %s", err)
+    http.Error(w, err.Error(), http.StatusInternalServerError)
   }
 
-  res, err := es.Search(
-    es.Search.WithIndex(indexName),
-    es.Search.WithBody(strings.NewReader(fmt.Sprintf(highlightQuery, "Item1", searchTerm, stockIndex))),
-  )
+  hits, err := client.highlightSearch(searchTerm, stockIndex, "Item1")
   if err != nil {
-    log.Fatalf("Error getting response: %s", err)
+    http.Error(w, err.Error(), http.StatusInternalServerError)
   }
-  defer res.Body.Close()
-	if res.IsError() {
-		var e map[string]interface{}
-		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-			log.Fatalf("Error parsing the response (with error) body: %s", err)
-		} else {
-			// Print the response status and error information.
-			log.Fatalf("[%s] %s: %s",
-				res.Status(),
-				e["error"].(map[string]interface{})["type"],
-				e["error"].(map[string]interface{})["reason"],
-			)
-		}
-	}
 
-  if res.Status() == "200 OK" {
-    body, err := io.ReadAll(res.Body)
-    if err != nil {
-      log.Fatalf("Error reading the response body: %s", err)
-    }
-    var resultBody ResultBody
-    if err = json.Unmarshal(body, &resultBody); err != nil {
-      log.Fatalf("Error parsing the response body: %s", err)
-    }
-    log.Printf("took: %v ms\n", resultBody.Took)
-    log.Printf("hits: %d\n", resultBody.Hits.Total.Num)
-    for i, hit := range resultBody.Hits.Values {
-      log.Printf("  id: %s, filed: %s, ticker: %s, index: %s\n", hit.Id, hit.Source.Filed,
-                  hit.Source.Ticker, hit.Source.StockIndex)
-      if i > 1 {
-        break
-      }
-    }
-    err = templates.ExecuteTemplate(&buf, "table.html", resultBody)
-    if err != nil {
-      http.Error(w, err.Error(), http.StatusInternalServerError)
-    }
+  err = templates.ExecuteTemplate(&buf, "table.html", hits)
+  if err != nil {
+    http.Error(w, err.Error(), http.StatusInternalServerError)
   }
-  /*
-  page := template.HTML(buf.String())
-  finalTpl := template.Must(template.New("final").Parse(`{{.}}`))
-  finalTpl.Execute(w, page)
-  */
+   
   fmt.Fprintf(w, "%s", buf.String())
 }
 
 func main() {
-  client_init()
+  client = NewElasticClient()
 	http.HandleFunc("/", httpserver)
 	//http.HandleFunc("/search", httpserver)
 	http.ListenAndServe(":8081", nil)
--- search.go
+++ search.go
@@ -7,33 +7,48 @@ import (
   "fmt"
   "encoding/json"
   "strings"
+  "html/template"
   "github.com/elastic/go-elasticsearch/v8"
 )
 
 const indexName = "filings"
 
-var es *elasticsearch.Client
-
-
 var histogramQuery = `{ 
-        "size": 0,
-        "query": { 
-              "bool": { 
-                "must": [{ "match_phrase": { "Item1": "%s" }}],
-                "filter": [{ "bool": { "must": [{ "term": { "StockIndex.keyword": "%s" }}]}}]}},
-        "aggs": { "year": { "date_histogram": { "field": "Filed", "calendar_interval": "1y"}}}
-    }`
+  "size": 0,
+  "query": { "bool": { 
+    "must": [{ "match_phrase": { "%s": "%s" }}],
+    "filter": [{ "bool": { "must": [{ "term": { "StockIndex.keyword": "%s" }}]}}]}},
+  "aggs": { "year": { "date_histogram": { "field": "Filed", "calendar_interval": "1y"}}}
+}`
+
+type HistogramResult struct {
+  Took float64 `json:"took"`
+  Hits struct {
+    Total struct {
+      Num int `json:"value"`
+    } `json:"total"`
+  }
+  Aggregations struct {
+    Year struct {
+      Buckets []struct {  
+        Date  string  `json:"key_as_string"`
+        Count float64 `json:"doc_count"`
+      } `json:"buckets"`
+    } `json:"year"`
+  } `json:"aggregations"`
+}
 
 var highlightQuery = `{ 
-              "_source": ["Ticker", "Name", "StockIndex", "Filed"],
-              "query": { "match_phrase": { "Item1": "%s" } },
-              "highlight": { "fragment_size": 200, "fields": { "Item1": {} } },
-              "sort": [ { "Filed": { "order": "desc", "unmapped_type": "date" } } ],
-              "size": 1000
-            }`
-// including highlight makes query ~3.5 times slower
+  "_source": ["Ticker", "Name", "StockIndex", "Filed"],
+  "query": { "bool": { 
+    "must": [{ "match_phrase": { "%s": "%s" }}],
+    "filter": [{ "bool": { "must": [{ "term": { "StockIndex.keyword": "%s" }}]}}]}},
+  "highlight": { "fragment_size": 200, "fields": { "Item1": {}, "Item1a": {} } },
+  "sort": [ { "Filed": { "order": "desc", "unmapped_type": "date" } } ],
+  "size": 30
+}`
 
-type ResultBody struct {
+type HighlightResult struct {
   Took float64 `json:"took"`
   Hits struct {
     Total struct {
@@ -49,16 +64,18 @@ type ResultBody struct {
         Filed      string
       } `json:"_source"`
       Highlights struct {
-        Item1  []string
-        Item1a []string
+        Item1  []template.HTML // so <em> is not escaped
+        Item1a []template.HTML
       } `json:"highlight"`
     } `json:"hits"`
   } `json:"hits"`
 }
 
-func client_init() {
-  var err error
+type ElasticClient struct {
+  es *elasticsearch.Client
+}
 
+func NewElasticClient() *ElasticClient {
   cfg := elasticsearch.Config {
     Addresses: []string {
       "https://localhost:9200",
@@ -67,80 +84,100 @@ func client_init() {
     Password: os.Getenv("ES_PASS"),
     CertificateFingerprint: os.Getenv("ES_CERTFP"),
   }
-
-  es, err = elasticsearch.NewClient(cfg)
+  es, err := elasticsearch.NewClient(cfg)
   if err != nil {
     log.Fatalf("Error creating client: %s", err)
   }
+  return &ElasticClient{es: es}
 }
 
+func (client *ElasticClient) histogramSearch(searchTerm, stockIndex string) (
+  map[string](map[string]int), error) {
 
-
-func main() {
-  client_init()
-
-  res, err := es.Search(
-    es.Search.WithIndex(indexName),
-    es.Search.WithBody(strings.NewReader(fmt.Sprintf(highlightQuery, "blockchain"))),
+  var (
+    err error
+    histogramResult HistogramResult
   )
-  if err != nil {
-    log.Fatalf("Error getting response: %s", err)
-  }
-  defer res.Body.Close()
-	if res.IsError() {
-		var e map[string]interface{}
-		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-			log.Fatalf("Error parsing the response (with error) body: %s", err)
-		} else {
-			// Print the response status and error information.
-			log.Fatalf("[%s] %s: %s",
-				res.Status(),
-				e["error"].(map[string]interface{})["type"],
-				e["error"].(map[string]interface{})["reason"],
-			)
-		}
-	}
+  counts := make(map[string](map[string]int))
 
-  if res.Status() == "200 OK" {
+  for _, field := range fields {
+    m := make(map[string]int)
+    res, err := client.es.Search(
+      client.es.Search.WithIndex(indexName),
+      client.es.Search.WithBody(strings.NewReader(
+        fmt.Sprintf(histogramQuery, field, searchTerm, stockIndex))),
+    )
+    if err != nil {
+      return counts, err
+    }
+    defer res.Body.Close()
+    if res.IsError() || res.Status() != "200 OK" {
+      err = fmt.Errorf("res.IsError or status not 200 OK")
+        return counts, err
+    }
+
     body, err := io.ReadAll(res.Body)
     if err != nil {
       log.Fatalf("Error reading the response body: %s", err)
+      return counts, err
     }
-    var resultBody ResultBody
-    if err = json.Unmarshal(body, &resultBody); err != nil {
-      log.Fatalf("Error parsing the response body: %s", err)
+    if err = json.Unmarshal(body, &histogramResult); err != nil {
+      return counts, err
     }
-    log.Printf("took: %v ms\n", resultBody.Took)
-    log.Printf("hits: %d\n", resultBody.Hits.Total.Num)
-    for i, hit := range resultBody.Hits.Values {
-      log.Printf("  id: %s, filed: %s, ticker: %s, index: %s\n", hit.Id, hit.Source.Filed,
-                  hit.Source.Ticker, hit.Source.StockIndex)
-      if i > 10 {
-        break
-      }
-    }
-  }
 
-  res, err = es.Search(
-    es.Search.WithIndex(indexName),
-    es.Search.WithBody(strings.NewReader(fmt.Sprintf(histogramQuery, "blockchain", "RUSSELL 2000"))),
-    es.Search.WithPretty(),
+    for _, b := range histogramResult.Aggregations.Year.Buckets {
+      year := b.Date[:4]
+      count := int(b.Count)
+      m[year] = count
+    }
+  counts[field] = m
+  }
+  return counts, err
+}
+
+func (client *ElasticClient) highlightSearch(searchTerm, stockIndex, field string) (
+  [](map[string]any), error) {
+
+  var (
+    hits [](map[string]any)
+    err error
+    highlightResult HighlightResult
+  )
+
+  res, err := client.es.Search(
+    client.es.Search.WithIndex(indexName),
+    client.es.Search.WithBody(strings.NewReader(
+      fmt.Sprintf(highlightQuery, field, searchTerm, stockIndex))),
   )
   if err != nil {
-    log.Fatalf("Error getting response: %s", err)
+    return hits, err
   }
   defer res.Body.Close()
-	if res.IsError() {
-		var e map[string]interface{}
-		if err := json.NewDecoder(res.Body).Decode(&e); err != nil {
-			log.Fatalf("Error parsing the response (with error) body: %s", err)
-		} else {
-			// Print the response status and error information.
-			log.Fatalf("res.IsError [%s] %s: %s",
-				res.Status(),
-				e["error"].(map[string]interface{})["type"],
-				e["error"].(map[string]interface{})["reason"],
-			)
-		}
-	}
+  if res.IsError() || res.Status() != "200 OK" {
+    err = fmt.Errorf("res.IsError or status not 200 OK")
+      return hits, err
+  }
+
+  body, err := io.ReadAll(res.Body)
+  if err != nil {
+    log.Fatalf("Error reading the response body: %s", err)
+    return hits, err
+  }
+  if err = json.Unmarshal(body, &highlightResult); err != nil {
+    return hits, err
+  }
+
+  for _, hit := range highlightResult.Hits.Values {
+    m := make(map[string]any)
+    m["Filed"] = hit.Source.Filed
+    m["Ticker"] = hit.Source.Ticker
+    m["Name"] = hit.Source.Name
+    if field == "Item1" {
+      m["Excerpt"] = hit.Highlights.Item1[0]
+    } else {
+      m["Excerpt"] = hit.Highlights.Item1a[0]
+    }
+    hits = append(hits, m)
+  }
+  return hits, err
 }
